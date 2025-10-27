75b6cd15bb11890dc6536ebccabe66ccdd621ffd
umi <jonathanhoang2002@gmail.com>
2025-10-26 23:23:37 -0400

    Revert changes to eeprom lib

--- src/eeprom.c
+++ src/eeprom.c
@@ -6,9 +6,10 @@ void EEPROMwrite(uint16_t address, uint8_t data) {
   /* Set up address and Data Registers */
   EEAR = address;
   EEDR = data;
-  /* Start eeprom write by setting EEMPE and EEPE */
-  setPin(&EECR, EEMPE);
-  setPin(&EECR, EEPE);
+  /* Start eeprom write by setting EEMPE and EEPE
+   * cannot use setPin function due to 4 clock cycle timing restrictions */
+  EECR |= (1 << EEMPE);
+  EECR |= (1 << EEPE);
 }
 
 uint8_t EEPROMread(uint16_t address) {
