17c5c83cabca4c1be135c4f1b33352c74ed7df63
umi <jonathanhoang2002@gmail.com>
2025-10-26 23:25:10 -0400

    Added eeprom usart example. Deleted build artifacts.

---
+++ examples/eeprom_usart/eeprom_usart.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include <umi.h>
+
+int main() {
+  USARTInit();
+
+  uint8_t message[] = "This is a message for the eeprom!\n";
+
+  for (uint8_t i = 0; i < sizeof(message) / sizeof(message[0]); i++) {
+    EEPROMwrite(i, message[i]);
+  }
+
+  for (uint8_t i = 0; i < sizeof(message) / sizeof(message[0]); i++) {
+    uint8_t value = EEPROMread(i);
+    USARTTransmit(value);
+  }
+}
