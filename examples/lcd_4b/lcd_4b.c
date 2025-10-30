d5963a77b16877d828f9bca1d4813095805f8f18
umi <jonathanhoang2002@gmail.com>
2025-10-29 20:42:21 -0400

    Display string message in lcd example

--- examples/lcd_4b/lcd_4b.c
+++ examples/lcd_4b/lcd_4b.c
@@ -3,6 +3,13 @@
 int main() {
   LCDInit4b();
 
+  uint8_t message[] = "LCD works!";
+
+  for (uint8_t i = 0; message[i] != '\0'; i++) {
+    while (readBusyFlag());
+    writeData4b(message[i]);
+  }
+
   while (1);
 
   return 0;
